8ce3e3e52b8729d8f20daf8a06b546f64c6869ae
Dennis Tijbosch <Kabouterhuisje@users.noreply.github.com>
2017-03-20 08:17:09 +0100

    added lang files

---
+++ lang/en.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+readonly EXIT_text="You lost the game! The correct answer was %s!\n"
+readonly INVALIDARG_text="Invalid argument!"
+readonly GUESSES_text="You have %s guesses left\n"
+readonly LASTGUESS_text="This is your last chance!\n"
+readonly LETTERS_text="Letters left to try: %s\n"
+readonly WORD_text="Word: %s\n"
+readonly INPUT_text="Next try? "
+readonly WIN_text="You win! You guessed the word %s with %d fault(s)!\n"
+
---
+++ lang/nl.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+readonly EXIT_text="Je hebt de game verloren! Het correcte antwoord was %s!\n"
+readonly INVALIDARG_text="Geen geldig argument!"
+readonly GUESSES_text="Je hebt nog %d kansen over\n"
+readonly LASTGUESS_text="Dit is je laatste kans!\n"
+readonly LETTERS_text="Letters die nog niet geprobeerd zijn: %s\n"
+readonly WORD_text="Woord: %s\n"
+readonly INPUT_text="Volgende keuze? "
+readonly WIN_text="Je wint! Je hebt het woord %s geraden met %d fout(en)!\n"
+
